ce308e24d5bc3fa1402c8c3e2c8438d765d5a07b
Gina Marieges <gina.marieges@gmail.com>
2023-05-29 15:43:42 +0200

    Add GlobalStyle in main

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
+import GlobalStyle from "./styles/GlobalStyle/GlobalStyle";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>
