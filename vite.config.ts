584f6c9c5da86445148d2de5155c245f5c53d72b
Gina Marieges <gina.marieges@gmail.com>
2023-05-29 18:12:19 +0200

    Add github workflows, audit and sonar

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,17 @@ export default defineConfig({
     include: ["./src/**/*.test.{ts,tsx}"],
     coverage: {
       provider: "c8",
+      reporter: ["lcov", "text"],
       all: true,
       src: ["src"],
-      exclude: ["**/types.ts", "**/*.d.ts"],
+      exclude: [
+        "**/types.ts",
+        "**/*.d.ts",
+        "src/components/App/App.tsx",
+        "src/styles/GlobalStyle/GlobalStyle.ts",
+        "src/main.tsx",
+        "src/store/index.ts",
+      ],
     },
   },
 });
