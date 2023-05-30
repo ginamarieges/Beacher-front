aa2b2568351746cd023b6ea038615ffe8ae7bb0f
Gina Marieges <gina.marieges@gmail.com>
2023-05-30 20:14:53 +0200

    Add userMocks file

---
+++ src/mocks/userMocks.ts
@@ -0,0 +1,7 @@
+import { UserTokenStructure } from "../store/user/types";
+
+export const userMock: UserTokenStructure = {
+  id: "23",
+  name: "Mambo",
+  token: "284ydo3i7q4uwhertofq837id4uhwerjpdo",
+};
