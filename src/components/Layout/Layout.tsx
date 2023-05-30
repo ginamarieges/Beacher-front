59f78f5409c15d8f2466e4825eed545c082daad6
Gina Marieges <gina.marieges@gmail.com>
2023-05-30 16:08:42 +0200

    Create LoginPage

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import ContainerStyled from "../shared/ContainerStyled";
 import Navbar from "../Navbar/Navbar.js";
 import Header from "../Header/Header.js";
 const Layout = (): React.ReactElement => {
+  const location = useLocation();
   return (
     <>
       <ContainerStyled>
-        <Header />
+        {location.pathname !== "/login" && <Header />}
         <Outlet />
       </ContainerStyled>
-      <Navbar />
+      {location.pathname !== "/login" && <Navbar />}
     </>
   );
 };
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,10 @@
+const LoginPage = (): React.ReactElement => {
+  return (
+    <>
+      <img src="./img/logo.svg" alt="Beacher logo" width={90} height={90} />
+      <h1 className="app-title">Beacher</h1>
+    </>
+  );
+};
+
+export default LoginPage;
