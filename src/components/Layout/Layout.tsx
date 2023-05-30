66d3bda3db9cb3eaf0c5e42220dfe16efc56a823
Gina Marieges <gina.marieges@gmail.com>
2023-05-30 12:00:36 +0200

    Add styles in header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import ContainerStyled from "../shared/ContainerStyled";
 import Navbar from "../Navbar/Navbar.js";
+import Header from "../Header/Header.js";
 const Layout = (): React.ReactElement => {
   return (
     <>
       <ContainerStyled>
+        <Header />
         <Outlet />
       </ContainerStyled>
-      <Navbar></Navbar>
+      <Navbar />
     </>
   );
 };
