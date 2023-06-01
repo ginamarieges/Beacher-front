4c181edf976a2071bfe93068ca62b030d4d7d2c4
Gina Marieges <gina.marieges@gmail.com>
2023-06-01 11:38:46 +0200

    Add useUser test with errorHandler

--- src/hooks/useUser/useUser.test.ts
+++ src/hooks/useUser/useUser.test.ts
@@ -3,11 +3,13 @@ import useUser from "./useUser";
 import { UserCredentials } from "../../store/user/types";
 import { userCredentialsMock } from "../../mocks/userMocks";
 import { tokenMock } from "../../mocks/userMocks";
+import { server } from "../../mocks/server";
+import { errorHandlers } from "../../mocks/handlers";
 
 describe("Given a useUser function", () => {
+  const userCredentials: UserCredentials = userCredentialsMock;
   describe("When the getUserToken function is called with a username and a password", () => {
     test("Then it should return a token", async () => {
-      const userCredentials: UserCredentials = userCredentialsMock;
       const expectedToken = tokenMock;
 
       const {
@@ -19,6 +21,24 @@ describe("Given a useUser function", () => {
       const token = await getUserToken(userCredentials);
 
       expect(token).toBe(expectedToken);
+    });
+  });
+
+  describe("When the getUserToken function is called with a valid username and an invalid password", () => {
+    test("Then it should return the response method status with a 401", () => {
+      server.resetHandlers(...errorHandlers);
+
+      const {
+        result: {
+          current: { getUserToken },
+        },
+      } = renderHook(() => useUser());
+
+      const getToken = async () => {
+        await getUserToken(userCredentials);
+      };
+
+      expect(getToken).rejects.toThrowError();
     });
   });
 });
