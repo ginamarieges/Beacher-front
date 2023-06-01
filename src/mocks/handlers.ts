4f906c63db7d8080e2534de547c9b883c3bbf682
Gina Marieges <gina.marieges@gmail.com>
2023-06-01 11:36:48 +0200

    Create errorHandler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -8,3 +8,9 @@ export const handlers = [
     return res(ctx.status(200), ctx.json({ token: tokenMock }));
   }),
 ];
+
+export const errorHandlers = [
+  rest.post(`${apiUrl}/user/login`, (_req, res, ctx) => {
+    return res(ctx.status(401));
+  }),
+];
