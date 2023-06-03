41d294b47abcaf21a9c8805eddf355a05cb6a680
Gina Marieges <gina.marieges@gmail.com>
2023-06-03 21:09:45 +0200

    Modify ListPage test to include username

--- src/pages/ListPage/ListPage.test.tsx
+++ src/pages/ListPage/ListPage.test.tsx
@@ -1,13 +1,27 @@
 import { screen } from "@testing-library/react";
-import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+import { renderWithProviders } from "../../utils/testUtils";
 import ListPage from "./ListPage";
+import { loginUser } from "../../mocks/userMocks";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
 
 describe("Given a LisPage page", () => {
   describe("When it is rendered", () => {
     test("Then it should show a heading with the text 'Welcome! Find your perfect beach for today'", () => {
-      const expectedText = "Welcome! Find your perfect beach for today";
+      const user = loginUser;
+      const expectedText = `Welcome ${user.name}! Find your perfect beach for today`;
 
-      renderWithProviders(wrapWithRouter(<ListPage />));
+      const routes = [
+        {
+          path: "/",
+          element: <ListPage />,
+        },
+      ];
+
+      const router = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={router} />, {
+        userStore: user,
+      });
 
       const heading = screen.getByRole("heading", { name: expectedText });
 
