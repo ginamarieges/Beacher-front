6ace07c9d3e41c014efa45b7f274226a668ccad1
Gina Marieges <gina.marieges@gmail.com>
2023-06-03 16:58:25 +0200

    Add ListPage

---
+++ src/pages/ListPage/ListPage.tsx
@@ -0,0 +1,11 @@
+import ListPageStyled from "./ListPageStyled";
+
+const ListPage = (): React.ReactElement => {
+  return (
+    <ListPageStyled>
+      <p>Welcome! Find your perfect beach for today</p>
+    </ListPageStyled>
+  );
+};
+
+export default ListPage;
