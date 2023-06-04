7e72d7971b07e3591cb4da852b033eea41524c7b
Gina Marieges <gina.marieges@gmail.com>
2023-06-04 15:30:53 +0200

    Modify styles in Button and BeachCard

--- src/components/Button/ButtonStyled.ts
+++ src/components/Button/ButtonStyled.ts
@@ -5,16 +5,6 @@ const ButtonStyled = styled.button`
   font-size: ${(props) => props.theme.fontSizes.smallSize};
   padding: 15px;
   text-transform: uppercase;
-
-  .light-button {
-    background-color: ${(props) => props.theme.color.secondary};
-    color: ${(props) => props.theme.color.primary};
-  }
-
-  .dark-button {
-    background-color: ${(props) => props.theme.color.primary};
-    color: ${(props) => props.theme.color.secondary};
-  }
 `;
 
 export default ButtonStyled;
