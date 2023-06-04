131d0572515dcebc0ccb208a89d3bd750977f0c9
Gina Marieges <gina.marieges@gmail.com>
2023-06-04 17:45:01 +0200

    Add new beachesFactory

--- src/mocks/beachesMocks.ts
+++ src/mocks/beachesMocks.ts
@@ -1,3 +1,8 @@
-import { getBeachesMock } from "./factories/beach/beachFactory";
+import {
+  getBeachesDataMock,
+  getBeachesMock,
+} from "./factories/beach/beachFactory";
 
 export const mockBeaches = getBeachesMock(3);
+
+export const mockBeachesData = getBeachesDataMock(3);
