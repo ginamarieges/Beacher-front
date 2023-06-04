c425ffe74d74da73da5e5a7d5fafd27fa3faef13
Gina Marieges <gina.marieges@gmail.com>
2023-06-04 18:07:55 +0200

    Add error case in useBeaches test

--- src/hooks/useBeaches/useBeaches.test.ts
+++ src/hooks/useBeaches/useBeaches.test.ts
@@ -3,6 +3,8 @@ import useBeaches from "./useBeaches";
 import { mockBeaches } from "../../mocks/beachesMocks";
 import { wrapper } from "../../utils/testUtils";
 import { BeachStructure } from "../../store/beaches/types";
+import { server } from "../../mocks/server";
+import { errorHandlers } from "../../mocks/handlers";
 
 describe("Given a useBeaches function", () => {
   describe("When the getBeaches function is called", () => {
@@ -17,6 +19,20 @@ describe("Given a useBeaches function", () => {
       const beaches = await getBeaches();
 
       expect(beaches.beaches).toStrictEqual(expectedBeaches);
+    });
+  });
+
+  describe("When the getBeaches function rejects", () => {
+    test("Then it should throw the 'Can't get the list of beaches' error", () => {
+      server.resetHandlers(...errorHandlers);
+
+      const {
+        result: {
+          current: { getBeaches },
+        },
+      } = renderHook(() => useBeaches(), { wrapper: wrapper });
+
+      expect(getBeaches()).rejects.toThrowError();
     });
   });
 });
