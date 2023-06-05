9d215858be8b4d916429ff3d4b3a8e9a385b39ec
Gina Marieges <gina.marieges@gmail.com>
2023-06-05 16:10:53 +0200

    Add showFeedback test

--- src/store/ui/uiReducer.test.ts
+++ src/store/ui/uiReducer.test.ts
@@ -1,5 +1,9 @@
-import { UiStructure } from "./types";
-import { hideLoaderActionCreator, showLoaderActionCreator } from "./uiSlice";
+import { UiFeedback, UiStructure } from "./types";
+import {
+  hideLoaderActionCreator,
+  showFeedbackActionCreator,
+  showLoaderActionCreator,
+} from "./uiSlice";
 import { uiReducer } from "./uiSlice";
 
 describe("Given a showLoader reducer", () => {
@@ -7,8 +11,14 @@ describe("Given a showLoader reducer", () => {
     test("Then it should return a new state with isLoading true", () => {
       const currentState: UiStructure = {
         isLoading: false,
+        isError: false,
+        message: "",
       };
-      const expectedNewState: UiStructure = { isLoading: true };
+      const expectedNewState: UiStructure = {
+        isLoading: true,
+        isError: false,
+        message: "",
+      };
       const action = showLoaderActionCreator();
 
       const newState = uiReducer(currentState, action);
@@ -16,14 +26,47 @@ describe("Given a showLoader reducer", () => {
       expect(newState).toStrictEqual(expectedNewState);
     });
   });
+});
 
+describe("Given a hideLoader reducer", () => {
   describe("When it receives a ui state with the property isLoading true", () => {
     test("Then it should return a new state with isLoading false", () => {
       const currentState: UiStructure = {
         isLoading: true,
+        isError: false,
+        message: "",
       };
-      const expectedNewState: UiStructure = { isLoading: false };
+      const expectedNewState: UiStructure = {
+        isLoading: false,
+        isError: false,
+        message: "",
+      };
       const action = hideLoaderActionCreator();
+
+      const newState = uiReducer(currentState, action);
+
+      expect(newState).toStrictEqual(expectedNewState);
+    });
+  });
+});
+
+describe("Given a showFeedback reducer", () => {
+  describe("When it receives a ui state with the message 'Wrong' and isError true", () => {
+    test("Then it should return a new state with the message 'Wrong' and isError true", () => {
+      const currentState: UiStructure = {
+        isLoading: true,
+        isError: false,
+        message: "",
+      };
+      const errorMessage = "Wrong";
+      const error: UiFeedback = { isError: true, message: errorMessage };
+      const expectedNewState: UiFeedback = {
+        ...currentState,
+        isError: error.isError,
+        message: error.message,
+      };
+
+      const action = showFeedbackActionCreator(error);
 
       const newState = uiReducer(currentState, action);
 
