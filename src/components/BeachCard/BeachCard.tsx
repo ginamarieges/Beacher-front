322ded252f5d1d3dd13c68c3a21a19c207386d19
Gina Marieges <gina.marieges@gmail.com>
2023-06-05 11:30:55 +0200

    Modify card style to add width and height in image

--- src/components/BeachCard/BeachCard.tsx
+++ src/components/BeachCard/BeachCard.tsx
@@ -7,7 +7,14 @@ interface BeachCardProps {
 const BeachCard = ({ beach: { image, name, town } }: BeachCardProps) => {
   return (
     <BeachCardStyled className="card">
-      <img className="card__image" loading="lazy" src={image} alt={name} />
+      <img
+        className="card__image"
+        loading="lazy"
+        src={image}
+        alt={name}
+        width={258}
+        height={85}
+      />
       <h4 className="card__name">{name}</h4>
       <span className="card__town">{town}</span>
     </BeachCardStyled>
