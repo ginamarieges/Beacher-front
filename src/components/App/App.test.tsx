18e9bd3a9237152e4960293605d826b9e4d7f75f
Gina Marieges <gina.marieges@gmail.com>
2023-06-08 10:50:25 +0200

    Move conditional rendering to layout

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,8 +2,8 @@ import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { UiStructure } from "../../store/ui/types";
 import { renderWithProviders } from "../../utils/testUtils";
-import App from "./App";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
 
 describe("Given a Modal component", () => {
   describe("When it is rendered and the button it's clicked", () => {
@@ -11,7 +11,7 @@ describe("Given a Modal component", () => {
       const routes = [
         {
           path: "/",
-          element: <App />,
+          element: <Layout />,
         },
       ];
 
@@ -19,8 +19,11 @@ describe("Given a Modal component", () => {
 
       const uiState: UiStructure = {
         isLoading: false,
-        isError: true,
-        message: "Something went wrong",
+        modal: {
+          isVisible: true,
+          isError: true,
+          message: "Something went wrong",
+        },
       };
       const buttonText = "close button";
 
