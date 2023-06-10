738c52f05cb3a54c130a06dbff8ae2de8317511b
Gina Marieges <gina.marieges@gmail.com>
2023-06-10 09:11:37 +0200

    Modify max-width of BeachCard

--- src/components/shared/ContainerStyled.ts
+++ src/components/shared/ContainerStyled.ts
@@ -4,7 +4,7 @@ const ContainerStyled = styled.div`
   margin: 0 auto;
   padding: 0 20px;
   min-height: 100vh;
-  max-width: 600px;
+  max-width: 400px;
   min-width: 320px;
   background-color: ${(props) => props.theme.color.secondary};
   padding-bottom: 90px;
