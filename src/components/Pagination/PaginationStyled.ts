32d0f59fd9d27a1b8253a00ecbfc9a382f0b751d
Gina Marieges <gina.marieges@gmail.com>
2023-06-10 14:36:00 +0200

    Add PaginationStyled and add component in ListPage

---
+++ src/components/Pagination/PaginationStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const PaginationStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  color: ${(props) => props.theme.color.primary};
+
+  .hidden {
+    visibility: hidden;
+  }
+`;
+
+export default PaginationStyled;
