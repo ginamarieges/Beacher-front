eb5eb4ed84dd82faaedcc056134594a5456e280c
Gina Marieges <gina.marieges@gmail.com>
2023-06-11 18:56:59 +0200

    Add Filter test

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -3,7 +3,12 @@ import FilterStyled from "./FilterStyled";
 const Filter = (): React.ReactElement => {
   return (
     <FilterStyled className="select">
-      <select className="select__input" id="region" name="region">
+      <select
+        className="select__input"
+        id="region"
+        name="region"
+        aria-label="region-selector"
+      >
         <option value="">Where do you want to go?</option>
         <option value="Alt Empordà">Alt Empordà</option>
         <option value="Baix Empordà">Baix Empordà</option>
