b75987dc2982d103b62cef237c4c141dcb8dc4fd
Gina Marieges <gina.marieges@gmail.com>
2023-06-13 12:13:35 +0200

    Add length in BeachStateStructure

--- src/store/beaches/types.ts
+++ src/store/beaches/types.ts
@@ -25,13 +25,9 @@ export interface BeachStateStructure {
   beaches: BeachStructure[];
   region?: string;
   beach: BeachStructure;
+  length: number;
 }
 
 export interface BeachStateDataStructure {
   beaches: BeachDataStructure[];
-}
-
-export interface BeachGetStateStructure {
-  beaches: BeachStructure[];
-  length: number;
 }
